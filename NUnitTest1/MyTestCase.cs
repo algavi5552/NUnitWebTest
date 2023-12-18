393a958595f0e05c8e9107667906960dea78c03b
Pavel <algave28@gmail.com>
2023-12-18 14:19:42 +0300

    Add project files.

---
+++ NUnitTest1/MyTestCase.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace NUnitWebTest
+{
+    [TestFixture]
+    class MyTestCase
+    {
+        [TestCase]
+        public void FirtsPageGreetingsTest()
+        {
+            var firstPageUrl = "https://my.life-pos.ru/auth/login";
+            var greetingsSelector = "body > app-root > app-auth > div > div > div.wrapper-container > div.content > div.column.router-portal > app-login > div > div.header > h2";
+            
+            var webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl(firstPageUrl);
+
+            var greetings = webDriver.FindElement(By.CssSelector(greetingsSelector));
+
+            bool dayCheck = greetings.Text.Contains("День") && UsedFunctions.DayNightCheck().Contains("day");
+            bool nightCheck = greetings.Text.Contains("Вечер") && UsedFunctions.DayNightCheck().Contains("night");
+
+            //одна из проверок должна пройти, вторая нет, так как сейчас либо день, либо ночь:
+            Assert.AreNotEqual(dayCheck && nightCheck, "Неверное отображение дня и ночи!");
+
+            webDriver.Close();
+            webDriver.Dispose();
+
+        }
+    }
+}
---
+++ NUnitTest1/UsedFunctions.cs
@@ -0,0 +1,20 @@
+﻿namespace NUnitWebTest
+{
+    public class UsedFunctions
+    {
+        public static string DayNightCheck()
+        {
+            const int dayStart = 8;
+            const int dayEnd = 20;
+
+            var currentHour = Convert.ToInt32(DateTime.Now.Hour);
+
+            if (currentHour > dayStart && currentHour < dayEnd)
+            {
+                return "day";
+            }
+            return "night";
+        }
+
+    }
+}
